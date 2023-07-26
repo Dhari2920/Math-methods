d39ebd8bd6cf15959acb25a4b8dbd3a1719f6d5c
Dhari2920 <dhari2920@gmail.com>
2023-07-26 21:56:04 +0530

    math.floor updated

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var countdown = 10;
-var i = 0;
-while(i < countdown)
-{
-    console.log(countdown);
-    countdown--;
-}
+//var countdown = 10;
+//var i = 0;
+//while(i < countdown)
+//{
+   // console.log(countdown);
+   // countdown--;
+//}
+console.log(Math.floor(5.9));
